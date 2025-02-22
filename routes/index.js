273406b758150af395eca0ebcbb691deabee5a5f
ikabeee <carlglz30@gmail.com>
2025-02-22 13:00:38 -0500

    feat: add vendor routes to main router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const router = express.Router();
+const vendorRouter = require('./VendorRoutes');
 const userRouter = require('./UserRoutes');
 
 router.use('/user', userRouter)
+router.use('/vendor', vendorRouter)
 
 module.exports = router;
