1a89210db01266fe3178d73cbadf67c2708a14a3
ikabeee <carlglz30@gmail.com>
2025-02-22 11:13:19 -0500

    feat: set up Express server with basic routing and middleware

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan  = require('morgan');
+const extraRoutes = require ('./routes');
+const express = require('express')
+// const auth = require('./middlewares/auth');
+const projectInfo = require('./package.json');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan);
+app.use(cors({origin: '*'}));
+app.use(express.json());
+
+app.get('/', (_req, res)=>{
+    return res.send({
+        app: 'ContabilidaApi',
+        version: projectInfo.version || '1.0.0',
+        description: projectInfo.description || 'API para el manejo de la contabilidad de una empresa'
+        
+    });
+});
+
+app.use('/', extraRoutes);
+app.use((__req, res, _next)=>{
+    return res.status(404).send({message: 'Service Not Found'});
+})
+
+const server = http.createServer(app);
+
+server.listen(port, () =>  {
+    console.log(`Server running on port ${port}`)
+})
