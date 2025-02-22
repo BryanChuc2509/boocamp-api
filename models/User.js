97056471d8e95918142b75cad62969662e48f560
ikabeee <carlglz30@gmail.com>
2025-02-22 10:20:52 -0500

    feat: add User model with attributes for user management

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const UserOf = (sequelize, DataTypes) => {
+    const User = sequelize.default('User', {
+        userId: {
+            type: DataTypes.INTEGER,
+            primary: true,
+            autoIncrement: true,
+            field: 'ID',
+        },
+        name: {
+            type: DataTypes.STRING,
+            field: 'NOMBRE',
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            field: 'APELLIDOS'
+        },
+        email: {
+            type: DataTypes.STRING,
+            field: 'CORREO',
+        },
+        password: {
+            type: DataTypes.STRING,
+            field: 'CONTRASENIA'
+        },
+    })
+};
