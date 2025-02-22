043720b892f94688a7dc74739a02b1ab6390f125
ikabeee <carlglz30@gmail.com>
2025-02-22 12:19:30 -0500

    feat: implement CRUD operations for users in UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const { USE } = require('sequelize/lib/index-hints');
 const { User } = require('../models')()
 
 dotenv.config()
@@ -14,57 +15,78 @@ const getUsers = async (__req, res) => {
     try {
         const users = await User.findAll();
         return res.send(users);
-    }catch(error){
+    } catch (error) {
         console.log(error);
-        return res.status(500).send({message: 'Error al obtener los usuarios'})
+        return res.status(500).send({ message: 'Error al obtener los usuarios' })
     }
 }
 
-const getUserById = async (userId) => {
+const getUserById = async (req, res) => {
     try {
+        const { userId } = req.params;
         const user = await User.findOne({
             where: {
-                userId,
+                userId: userId,
             }
         })
+        if(!user) {
+            return res.status(404).json({message: 'Error al encontrar el usuario'});
+        }
         return res.send(user)
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return null;
     }
 }
 
-// const create = async (req, res) => {
-//     try {
-//         const user = await User.create(req.body);
-//         return res.send({message:  'Usuario creado existosamente', data: user });
-//     } catch(error) {
-//         return res.status(500).json({message: 'Error al crear el usuario'});
-//     }
-// }
+const create = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        return res.send({ message: 'Usuario creado existosamente', data: user });
+    } catch (error) {
+        return res.status(500).json({ message: 'Error al crear el usuario' });
+    }
+}
 
 
-// const update = async (req, res) => {
-//     try {
-//         const {userId} = req.params;
-//         const userUpdated = await User.update(req.body, {
-//             where: { id },
-//         })
+const update = async (req, res) => {
+    try {
+        const { userId } = req.params;
 
-//         if(!userUpdated) {
-//             return res.status(404).json({message: 'Error al encontrar el usuario'});
-//         }
+        const user = await User.findOne({ where: { userId: userId } })
+        if (!user) {
+            return res.status(404).json({ message: 'Error al encontrar el usuario' });
+        }
+        const userUpdated = await User.update(req.body, {
+            where: { userId: userId },
+        })
+        return res.send({ message: 'Usuario actualizado existosamente', data: userUpdated });
+    } catch (error) {
+        return res.status(500).send({ message: 'Error al crear el usuario' });
+    }
+}
 
-//         return res.send({message:  'Usuario actualizado existosamente', data: User });
-//     } catch(error) {
-//         return res.status(500).send({message: 'Error al crear el usuario'});
-//     }
-// }
+const deleteUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findOne({ where: { userId: userId } })
+
+        if (!user) {
+            return res.status(404).json({ message: 'Error al encontrar el usuario' });
+        }
+        await User.destroy({where: {userId: userId}})
+        return res.status(201).send({message: 'Usuario eliminado'})
+
+    } catch (error) {
+        return res.status(500).send({ message: 'Error al eliminar el usuario' });
+    }
+}
 
 module.exports = {
     // login,
     getUsers,
     getUserById,
-    // create,
-    // update,
+    create,
+    update,
+    deleteUser
 };
