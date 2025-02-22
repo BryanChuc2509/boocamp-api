c72734e64a42853c363bac0ec0a7c6b1528e3672
ikabeee <carlglz30@gmail.com>
2025-02-22 13:00:49 -0500

    feat: add VendorRoutes for managing vendor operations

---
+++ routes/VendorRoutes.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const router = express.Router();
+const vendorsController = require('../controllers/VendorsController');
+
+router
+    .route('/all')
+    .get(vendorsController.getVendors);
+router
+    .route('/:vendorId')
+    .get(vendorsController.getVendorById)
+router
+    .route('/create')
+    .post(vendorsController.create)
+router
+    .route('/update/:vendorId')
+    .put(vendorsController.update)
+// router
+//     .route('/delete/:userId')
+//     .delete(vendorsController.deleteUser)
+
+module.exports = router;
