a77b288ad3d961a93e1ca5f21b403fbe08b7ee02
ikabeee <carlglz30@gmail.com>
2025-02-22 10:11:30 -0500

    refactor: improve model initialization and loading in index.js

--- models/index.js
+++ models/index.js
@@ -1,20 +1,21 @@
 const path = require('node:path');
 const dotenv = require('dotenv');
-const {readdirSync} = require('node:fs');
-const { Sequelize, DataTypes } =require('sequelize');
+const { readdirSync } = require('node:fs');
+const { Sequelize, DataTypes } = require('sequelize');
+const { default: ModelManager } = require('sequelize/lib/model-manager');
 
 dotenv.config();
 let sequelize;
 const db = {}
 
-const dbName=process.env.DB_NAME
-const dbUser=process.env.DB_NAME
-const dbPassword=process.env.DB_PASSWORD
-const dbHost=process.env.DB_HOST
-const dbPort=process.env.DB_PORT
+const dbName = process.env.DB_NAME
+const dbUser = process.env.DB_NAME
+const dbPassword = process.env.DB_PASSWORD
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
 
 const init = () => {
-    if(!sequelize){
+    if (!sequelize) {
         sequelize = new Sequelize({
             host: dbHost,
             username: dbUser,
@@ -29,4 +30,21 @@ const init = () => {
             }
         })
     }
+    const files = readdirSync(__dirname)
+        .filter((file) => {
+            return file.endsWith(".js") && file !== 'index.js';
+        });
+    files.forEach((file) => {
+        const model = require(path.join(__dirname, file)(sequelize, DataTypes));
+        db[model.name] = model;
+    });
+
+    Object.keys(db).forEach((modelName) => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    })
+    return db;
 }
+
+module.exports = init;
