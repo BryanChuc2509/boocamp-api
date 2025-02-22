5f69f93572025868f8246da54e732ca7ce3368ec
ikabeee <carlglz30@gmail.com>
2025-02-22 12:19:46 -0500

    fix: correct route path for user router in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require ('express');
 const router = express.Router();
 const userRouter = require('./UserRoutes');
 
-router.use('user', userRouter)
+router.use('/user', userRouter)
 
 module.exports = router;
