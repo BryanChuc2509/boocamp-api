3e3738aa465cad05dfac86162b6dd8bd53ec5577
ikabeee <carlglz30@gmail.com>
2025-02-28 07:57:04 -0500

    feat: add login and registration routes to the main router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require ('express');
 const router = express.Router();
 const vendorRouter = require('./VendorRoutes');
 const userRouter = require('./UserRoutes');
+const {login, create} = require('../controllers/UsersController')
 
+router.use('/login', login)
+router.use('/register', create)
 router.use('/user', userRouter)
 router.use('/vendor', vendorRouter)
+
 
 module.exports = router;
