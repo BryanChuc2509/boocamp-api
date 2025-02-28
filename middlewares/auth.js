7d7c72c8822b22b9a84a7814d70306d2b91e8da4
ikabeee <carlglz30@gmail.com>
2025-02-28 08:55:10 -0500

    feat: enhance authentication middleware to handle authorization errors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,6 +8,14 @@ const secret = process.env.JWT_SECRET;
 const auth = (req, res, next) => {
     const notAuthRequired = ['/', '/login', '/register'];
     if(notAuthRequired.includes(req.url)) return next();
-    const auth = req.headers;
-    if(!auth) return res.status(403)
-}
+    const { authorization } = req.headers;
+    if(!authorization) return res.status(403).send({ message: 'Access Denied', details: [{token: 'Not provided'}]});
+    try {
+        jwt.verify(auth.split(' ')[1], secret);
+    } catch(error) {
+        return res.status(403).send({ message: 'Access Denied', details: [{token: 'Invalid'}]});
+    }
+    return next();
+};
+
+module.exports =auth;
